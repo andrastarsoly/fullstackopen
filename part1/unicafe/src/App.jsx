cb00121348c960e396fea32f5231571f8539c27d
andras_tarsoly <tarsoly.andras.mark@gmail.com>
2024-02-19 21:50:59 +0100

    1.6 unicafe step1

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,19 +2,11 @@ import { useState } from 'react'
 
 
 
-const SectionHeader = (props) => {
-  return(
-    <div>
-      <h1>{props.title}</h1>
-    </div>
-  )
-}
+const SectionHeader = ({ title }) => <h1>{title}</h1>
 
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
-const handleClick = () => {
-
-}
-
+const Stat = ({ text, counter }) => <p> {text} {counter}</p>
 
   
 
@@ -25,10 +17,30 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGoodClick = () => setGood(good + 1)
+
+  const handleNeutralClick = () => setNeutral(neutral + 1)
+
+  const handleBadClick = () => setBad(bad + 1)
+
+
   return (
     <div>
       <SectionHeader title = "give feedback"/>
+      <Button 
+        onClick={handleGoodClick}
+        text="good"
+      />
+      <Button onClick={handleNeutralClick}
+        text="neutral"
+      />
+      <Button onClick={handleBadClick}
+        text="bad"
+      />
       <SectionHeader title = "statistics"/>
+      <Stat text="good" counter={good}/>
+      <Stat text="neutral" counter={neutral}/>
+      <Stat text="bad" counter={bad}/>
     </div>
   )
 }
