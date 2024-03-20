af198a54cc2f9bc1b49a1d74b87f9fa9e54829ce
andras_tarsoly <tarsoly.andras.mark@gmail.com>
2024-03-20 10:34:17 +0100

    2.6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -6,19 +6,40 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+    }
+  
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
-        <div>
-          name: <input />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <form onSubmit={addPerson}>
+        name:
+        <input
+          value={newName}
+          onChange={handleNameChange}
+        />
+        <button type="submit">add</button>
+      </form>   
       <h2>Numbers</h2>
-      ...
+      <ul>
+        {persons.map(person => <li key={person.name}> {person.name}</li>)}
+      </ul>
+
     </div>
   )
 }
