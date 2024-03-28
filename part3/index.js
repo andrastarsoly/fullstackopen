d1a31b4459f415dd7f7b6b73f2c6ad45127fb646
andras_tarsoly <tarsoly.andras.mark@gmail.com>
2024-03-28 19:25:40 +0100

    3.1

--- part3/index.js
+++ part3/index.js
@@ -26,14 +26,10 @@ const persons = [
 
 app.use(express.json())
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
 
-app.get('/api/persones', (request, response) => {
-  response.json(persones)
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
 })
-
 
 const PORT = 3001
 app.listen(PORT, () => {
