d9c468a0618f880c897b97d9b2531a5044c5df64
andras_tarsoly <tarsoly.andras.mark@gmail.com>
2024-03-28 19:41:26 +0100

    3.2

--- part3/index.js
+++ part3/index.js
@@ -26,10 +26,13 @@ const persons = [
 
 app.use(express.json())
 
-
 app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
+
+app.get('/info', (request, response) => {
+    response.send(`Phonebook has info for ${persons.length} people`)
+  })
 
 const PORT = 3001
 app.listen(PORT, () => {
